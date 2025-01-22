edb5d424ebbd83e4dc28a58e262783dcd0baedb8
bsharshitha98 <b.s.harsh29may@gmail.com>
2025-01-22 15:44:17 +0530

    run command added

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, "public")));
+const PORT = process.env.PORT || 3001;
+
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    setHeaders: (res, path) => {
+      res.setHeader("Referrer-Policy", "strict-origin");
+    },
+  })
+);
+
 app.use((req, res, next) => {
   res.setHeader("Referrer-Policy", "strict-origin");
+  console.log("hello");
   next();
 });
 
