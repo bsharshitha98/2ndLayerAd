ae029e297272c0c7e86147ca2b3774b81787b513
Harshitha <61576196+bsharshitha98@users.noreply.github.com>
2025-01-24 10:36:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(
   express.static(path.join(__dirname, "public"), {
     setHeaders: (res, path) => {
-      res.setHeader("Referrer-Policy", "strict-origin");
+      res.setHeader("Referrer-Policy", "no-referrer");
     },
   })
 );
